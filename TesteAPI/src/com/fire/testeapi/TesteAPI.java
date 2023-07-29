f187fa3901f0a7cf4f8d9c1809f78816d481f236
danilojbs <firethekill@outlook.com>
2023-07-29 16:02:28 -0300

    V1 - TesteAPI
    versão com gradle

---
+++ TesteAPI/src/com/fire/testeapi/TesteAPI.java
@@ -0,0 +1,13 @@
+package com.fire.testeapi;
+
+public class TesteAPI {
+
+	public static void cmdTesteAPI() {
+		System.out.println("§aTestou o cmdTesteAPI");
+	}
+	
+	public static String testeCmdTesteAPI() {
+		return "retornoTesteAPI";
+	}
+	
+}
---
+++ TesteAPI/src/com/fire/testeapi/methods/Methods.java
@@ -0,0 +1,12 @@
+package com.fire.testeapi.methods;
+
+public class Methods {
+
+	public static void testeMethods() {
+		System.out.println("§aTestou o testeMethods");
+	}
+	
+	public static String testeMethodsString() {
+		return "retornoMethods";
+	}
+}
