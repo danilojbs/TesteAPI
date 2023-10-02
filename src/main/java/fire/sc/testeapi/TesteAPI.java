a0dab21fa5e388139edc5ab0afbfe8ce6151b54c
danilojbs <firethekill@outlook.com>
2023-10-01 21:45:49 -0300

    segunda versao da API sem onEnable

--- src/main/java/fire/sc/testeapi/TesteAPI.java
+++ src/main/java/fire/sc/testeapi/TesteAPI.java
@@ -1,22 +1,10 @@
 package fire.sc.testeapi;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TesteAPI extends JavaPlugin {
+public final class TesteAPI {
 
-    @Override
-    public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage("&aPlugin Habilitado!");
-    }
-
-    @Override
-    public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage("&cPlugin Desabilitado!");
-    }
-
-    public String getName(Player p){
+    public static String getName(Player p){
         return p.getName();
     }
 
